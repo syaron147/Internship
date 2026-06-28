c628cb8197c651523df416e04cfb6c8210052f10
syaron147 <syaronkulungrai@gmail.com>
2026-06-28 23:00:57 +0545

    condition

---
+++ day7.js
@@ -0,0 +1,83 @@
+//switch condition statement
+
+// let day = Number(prompt("enter the day number:"));
+// switch(day){
+//     case 1:
+//         console.log("sunday");
+//         break;
+//         case 2:
+//             console.log("monday");
+//             break;
+//             case 3:
+//                 console.log("tuesday");
+//                 break;
+//                 case 4:
+//                     console.log("wednesday");
+//                     break;
+//                     case 5:
+//                         console.log("thursday");
+//                         break;
+//                         case 6:
+//                             console.log("friday");
+//                             break;
+//                             case 7:
+//                                 console.log("saturday");
+//                                 break;
+//                                 default:
+//                                     console.log("invalid day");
+// }
+
+// let choice = Number(prompt("enter operation\1, Addition\n2, Subtraction\n3, Multiplication\n4, Division\n5, Modulus"));
+
+// let num1=Number(prompt("enter your number"));
+// let num2=Number(prompt("enter your another number"));
+
+// switch(choice){
+//     case 1:
+//     console.log("Addition of two numbers is: "+(num1+num2));
+//     break;
+//     case 2:
+//         console.log("Subtraction of two numbers is: "+(num1-num2));
+//         break;
+//         case 3:
+//             console.log("Multiplication of two numbers is: "+(num1*num2));
+//             break;
+//             case 4:
+//                 console.log("Division of two numbers is: "+(num1/num2));
+//                 break;
+//                 default:
+//                     console.log("Invalid operation");
+// }
+    
+// let marks=Number(prompt("enter your marks"));
+
+// switch(Math.floor(marks/10)){
+//     case 10:
+//         case 9:
+//             console.log("A +");
+//             break;
+//             case 8:
+//                 console.log("A");
+//                 break;
+//                 case 7:
+//                     console.log("B");
+//                     break;
+//                     default:
+//                         console.log("Fail");
+// }
+
+//loops
+
+for (let i=1; i<=100; i++){
+console.log("oni-chan")
+}
+
+
+//sum of naturam number
+let num =77
+let sum=0;
+for(let i=1; i<=num; i++){
+    sum=sum+i;
+}
+console.log("sum of natural number is"+sum)
+
