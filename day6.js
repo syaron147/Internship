9f344948694804bf4cb1f143daeee9eeb29d7baf
syaron147 <syaronkulungrai@gmail.com>
2026-06-28 18:24:03 +0545

    java script

---
+++ day6.js
@@ -0,0 +1,83 @@
+//var name = "syaron:;"
+//let age = 19;
+//age = 20;
+//console.log(age);
+
+
+//datatype
+let number = 22.77; //int
+const name ="syaron"; //string
+let isStudent = true; //boolean
+let city; //undefined value
+let user = null; //null 
+console.log(typeof name);
+console.log(typeof number);
+console.log(typeof isStudent);
+console.log(typeof city);
+console.log(typeof user);
+
+//operations
+//arithmetic operations
+let x=4;
+let y=7;
+let sum=x+y
+console.log(sum)
+
+let p=27;
+let q=13;
+console.log(p-q);
+console.log(p*q);
+console.log(p/q);
+console.log(p%q);
+
+//comparion operators
+// ==
+// !=
+// >
+// <
+// >=
+// <=
+// === strict equal to
+
+let age = 22;
+console.log(age==22); 
+console.log(age!=67);
+console.log(age>=8);
+console.log(age<=22);
+
+
+
+//logical operators
+//%%
+let a=19
+let citizen=true;
+console.log(a>18 && citizen==true);
+
+
+//assigment operators is all about increament and decrement
+let a=5;
+a-=20;
+console.log(a)
+
+//conditions
+let age44=22;
+
+//voting is elegible or not
+if(age44>=18){
+    console.log("you are elegible for voting");
+} 
+else {
+    console,log("you are not elegible for voting");
+}
+
+//else if
+let mern=60;
+if(mern>=90){
+    console.log("A grade");
+}
+else if(mern>=80){
+    console.log("B grade");
+}
+else{
+    console.log("C grade");
+}
